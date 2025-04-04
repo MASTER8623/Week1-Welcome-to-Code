df536ccb02c301c7685bf490b98fac66c7e6b4ba
MASTER8623 <parinaido92@gmail.com>
2025-04-05 02:22:34 +0800

    push soal1

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,41 @@
+// Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
+
+// Gunakan switch case untuk kasus ini!
+
+// Contoh:
+
+// let hari = 21; let bulan = 1; let tahun = 1945;
+
+// Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
+
+//tips gunakan keyword ini di google "conditional switch case javascript"
+//dan mulailah membaca dan mencontek di stackoverflow xixixi
+
+
+const prompt = require('prompt-sync')();
+
+let tanggal = prompt("masukkan tanggal 1-31: ");
+let bulan = prompt("masukkan bulan 1-12: ");
+let tahun = prompt("masukkan tahun: ");
+
+
+const bulanNames = {
+    1: "Januari",
+    2: "Februari",
+    3: "Maret",
+    4: "April",
+    5: "Mei",
+    6: "Juni",
+    7: "Juli",
+    8: "Agustus",
+    9: "September",
+    10: "Oktober",
+    11: "November",
+    12: "Desember"
+};
+
+let formattedDate = `${tanggal} ${bulanNames[bulan]} ${tahun}`;
+console.log(formattedDate);
+
+
+//code switch case kamu disini
